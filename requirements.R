8fb998fdf5aed99e33276327b8ef9331287d76a1
Chester Ismay <chester@datacamp.com>
2017-09-09 12:51:43 -0700

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,3 +1,4 @@
 devtools::install_version("tidyverse", "1.1.1")
 devtools::install_version("gapminder", "0.2.0")
+devtools::install_version("fivethirtyeight", "0.3.0")
 devtools::install_github("dgrtwo/broom")
